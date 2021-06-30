e8a1370d0fb11b07b5af09792ff1b510e0f20e7d
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2021-06-30 20:32:47 +0200

    First Steps in OOP - Lab

---
+++ First Steps in OOP - Lab/01. Rhombus of Stars.py
@@ -0,0 +1,12 @@
+def print_row(size, star_count):
+    for row in range(size - star_count):
+        print(" ", end="")
+    for row in range(1, star_count):
+        print("*", end=" ")
+    print("*")
+size = int(input())
+for star_count in range(1, size):
+  print_row(size, star_count)
+for star_count in range(size, 0, -1):
+  print_row(size, star_count)
+
