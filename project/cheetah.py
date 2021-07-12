9f2ee398f48c70088a54f33975ecc9414a36d0b2
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2021-07-12 13:01:48 +0200

    Delete project directory

--- project/cheetah.py
+++
@@ -1,6 +0,0 @@
-from project.animal import Animal
-MONEY_FOR_CARE = 60
-class Cheetah(Animal):
-    def __init__(self, name: str, gender: str, age: int) -> None:
-        super().__init__(name, gender, age,60)
-        
--- project/lion.py
+++
@@ -1,8 +0,0 @@
-from project.animal import Animal
-MONEY_FOR_CARE = 50
-class Lion(Animal):
-    def __init__(self, name: str, gender: str, age: int) -> None:
-        super().__init__(name, gender, age, 50)
-        
-
-        
