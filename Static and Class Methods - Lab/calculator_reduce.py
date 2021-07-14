6884dcf7cebce75b701cf665971507d3fb3bb670
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2021-07-14 10:42:24 +0200

    Update calculator_reduce.py

--- Static and Class Methods - Lab/calculator_reduce.py
+++ Static and Class Methods - Lab/calculator_reduce.py
@@ -2,60 +2,22 @@ from functools import reduce
 
 
 class Calculator:
-    def add(*args):
+    @classmethod
+    def add(cls, *args):
         result = reduce(lambda x, y: x + y, args)
         return result
 
-    def multiply(*args):
+    @classmethod
+    def multiply(cls, *args):
         result = reduce(lambda x, y: x * y, args)
         return result
 
-    def divide(*args):
+    @classmethod
+    def divide(cls, *args):
         result = reduce(lambda x, y: x / y, args)
         return result
 
-    def subtract(*args):
+    @classmethod
+    def subtract(cls, *args):
         result = reduce(lambda x, y: x - y, args)
         return result
-    # def add(*args):
-    #     result = 0
-    #     for el in args:
-    #         result += el
-    #     return result
-    #
-    # def multiply(*args):
-    #     result = 1
-    #     for el in args:
-    #         result *= el
-    #     return result
-    #
-    # def divide(*args):
-    #     result = 0
-    #     counter = 0
-    #     for el in args:
-    #         counter += 1
-    #         if counter == 1:
-    #             result = el
-    #         else:
-    #             result /= el
-    #     return result
-    #
-    # def subtract(*args):
-    #     result = 0
-    #     counter = 0
-    #     for el in args:
-    #         counter += 1
-    #         if counter == 1:
-    #             result = el
-    #             continue
-    #         if el >= 0:
-    #             result -= el
-    #         else:
-    #             result += abs(el)
-    #     return result
-
-
-print(Calculator.add(5, 10, 4))
-print(Calculator.multiply(1, 2, 3, 5))
-print(Calculator.divide(100, 2))
-print(Calculator.subtract(90, 20, -50, 43, 7))
