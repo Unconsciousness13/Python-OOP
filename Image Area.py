92f208428bc73cff375f0ca39c52f83032d2987a
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2021-07-17 17:44:10 +0200

    Polymorphism and Abstraction - Lab

---
+++ Image Area.py
@@ -0,0 +1,16 @@
+class ImageArea:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def __gt__(self, other):
+        return self.get_area() > other.get_area()
+
+    def __ge__(self, other):
+        return self.get_area() >= other.get_area()
+
+    def __eq__(self, other):
+        return self.get_area() == other.get_area()
