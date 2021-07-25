ef499669ce9b7dd4eec953df799a36328e94f2a9
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2021-07-25 08:36:14 +0200

    Iterators and Generators - Lab

---
+++ Iterators and Generators - Lab/Vowels.py
@@ -0,0 +1,24 @@
+class vowels:
+    _vowels = {'a', 'e', 'o', 'y', 'u', 'i', 'I', 'A', 'E', 'O', 'Y', 'U'}
+
+    def __init__(self, text):
+        self.text = text
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= len(self.text):
+            raise StopIteration
+        ch = self.text[self.index]
+        self.index += 1
+        if ch not in self._vowels:
+            return self.__next__()
+
+        return ch
+
+
+my_string = vowels('Abcedifuty0o')
+for char in my_string:
+    print(char)
