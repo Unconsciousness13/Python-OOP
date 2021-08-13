13af08ffd4ba9a01f7def3003aeb161b02fce2d4
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2021-08-13 13:00:48 +0200

    Delete express_software.py

--- software/express_software.py
+++
@@ -1,6 +0,0 @@
-from project.software.software import Software
-
-
-class ExpressSoftware(Software):
-    def __init__(self, type, capacity_consumption, memory_consumption):
-        super().__init__(type, "Express", capacity_consumption, int(memory_consumption * 2))
