d6391f8ee12d63a5a46436393e5cfa57b73eb602
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2021-08-13 13:00:54 +0200

    Delete software directory

--- software/light_software.py
+++
@@ -1,6 +0,0 @@
-from project.software.software import Software
-
-
-class LightSoftware(Software):
-    def __init__(self, type, capacity_consumption, memory_consumption):
-        super().__init__(type, "Light", int(capacity_consumption * 1.5), int(memory_consumption * 0.5))
